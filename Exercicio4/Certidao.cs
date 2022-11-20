df82a562428786175c86cd13a216dbccc7256d9e
Isaac Farias <isaacfarias006@gmail.com>
2022-11-20 12:15:58 -0300

    Adiciona exercício 4

---
+++ Exercicio4/Certidao.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio4
+{
+    public class Certidao
+    {
+        private readonly Pessoa pessoa;
+
+        private readonly DateTime dataEmissao;
+
+        public Certidao(Pessoa pessoa)
+        {
+            if (pessoa.Certidao != null)
+                throw new Exception("Essa pessoa já tem certidão");
+
+            this.pessoa = pessoa;
+            dataEmissao = DateTime.Today;
+
+            pessoa.AddCertidao(this);
+        }
+
+        public Pessoa Pessoa { get { return pessoa; } }
+
+        public DateTime DataEmissao { get { return dataEmissao; } }
+    }
+}
---
+++ Exercicio4/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio4
+{
+    public class Pessoa
+    {
+        private string nome;
+
+        private Certidao? certidao;
+
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string Nome { get { return nome; } }
+
+        public Certidao? Certidao { get { return certidao; } }
+
+        public void AddCertidao(Certidao certidao)
+        {
+            if (Certidao != null)
+                throw new Exception("Essa pessoa já tem certidão");
+
+            this.certidao = certidao;
+        }
+    }
+}
